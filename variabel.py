4c20c980af38aac1574d5806a9401c561a3c7d21
TheJarAiCyberCode <thejaraicybercode@gmail.com>
2025-09-17 07:21:29 +0700

    Add New File variabel.py

---
+++ variabel.py
@@ -0,0 +1,51 @@
+# Variabel untuk penanda agar struktur output script mudah dibaca
+start_script = "#=====> Script variabel.py dimulai <=====#"
+end_script = "#=====> Script selesai dijalankan <=====#"
+
+print (start_script)
+#========================================#
+
+# Variabel Python
+""" Variabel adalah sebuah tempat untuk menyimpan nilai data """
+
+# Membuat variabel
+variabel_string = "The JarAi Cyber Code"
+variabel_integer = 10
+variabel_float = 10.5
+variabel_bool = True
+
+# Mencetak atau menampilkan variabel dengan print()
+print ("Isi dari variabel_string adalah:", variabel_string)
+print ("Isi dari variabel_integer adalah:", variabel_integer)
+print ("Isi dari variabel_float adalah:", variabel_float)
+print ("Isi dari variabel_bool adalah:", variabel_bool)
+
+# Mengetahui tipe data suatu variabel dengan fungsi type()
+print ("Tipe data variabel_float adalah:", type(variabel_float))
+
+# Tanda kutip tunggal atau ganda ?
+"""
+Variabel string bisa di deklarasikan dengan menggunakan
+tanda kutip tunggal dan ganda itu sama saja. Contohnya:
+
+variabel_string = 'The JarAi Cyber Code'
+variabel_string = "The JarAi Cyber Code"
+"""
+
+# Peka terhadap huruf kecil dan besar
+"""
+Nama suatu variabel peka huruf kecil dan besar. Contohnya:
+
+ini akan membuat dua variabel
+variabel = 8
+Variabel = "JarAi"
+"""
+
+# Penamaan variabel
+"""
+nama_variabel = 10 # menggunakan underscore/garis bawah (snake_case) <== Disarankan!
+namaVariabel = 3 # huruf kecil diawal, huruf kapital awal kata berikutnya. (camelCase)
+variabel3 = 5 # menggunakan kombinasi angka di akhir
+"""
+#========================================#
+print (end_script)
