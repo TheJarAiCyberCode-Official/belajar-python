c956412a6881442a7da8cc60639934a704756a59
TheJarAiCyberCode <thejaraicybercode@gmail.com>
2025-09-20 19:21:15 +0700

    modified:   casting_tipe_data.py 	modified:   main.py 	modified:   tipe_data.py 	modified:   variabel.py

--- variabel.py
+++ variabel.py
@@ -1,7 +1,9 @@
 # Modul os digunakan untuk berinteraksi dengan sistem operasi.
 import os
 
-# Mengambil nama file dari variabel __file__
+# Mengambil bagian nama file saja dari path lengkap yang ada di __file__.
+# jika __file__ isinya /home/TheJarAiCyberCode/script/namafile.py,
+# maka os.path.basename() akan mengambil namafile.py saja.
 nama_file = os.path.basename(__file__)
 
 # Untuk penanda agar struktur output skrip mudah dibaca
