a4f168b9fb705898a6423d13fb19839427b1800c
TheJarAiCyberCode <thejaraicybercode@gmail.com>
2025-09-20 12:53:53 +0700

    add	new file:   main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Modul os digunakan untuk berinteraksi dengan sistem operasi.
+import os
+
+# Mengambil nama file dari variabel __file__
+nama_file = os.path.basename(__file__)
+
+# Untuk penanda agar struktur output skrip mudah dibaca
+print (f"==> Skrip {nama_file} dimulai <==")
+print ("----------------------------------------")
+#=======================================================#
+#         Tempat kamu memulai koding disini!
+#=======================================================#
+print ("----------------------------------------")
+print (f"==> Skrip {nama_file} selesai dijalankan <==")
